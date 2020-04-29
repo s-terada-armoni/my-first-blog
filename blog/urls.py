1225e7e79491e707ac1a54b718dc6d330a09c2e7
S.Terada <tstuba1220+office@gmail.com>
2020-04-29 21:21:50 +0900

    Added view and template for detailde blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # デフォルトサイトにアクセスしてきたらviews.post_listが行き先
     path('', views.post_list, name='post_list'),
+
+    # http://127.0.0.1:8000/post/5/ などのURLが要求されたら、post_detailが行き先
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
 ]
